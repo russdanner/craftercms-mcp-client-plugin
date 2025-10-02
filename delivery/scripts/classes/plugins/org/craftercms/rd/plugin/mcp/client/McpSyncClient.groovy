9999fc5cccfd0b0b72e3b51890197332f6515a7d
russdanner <russ.danner@craftercms.com>
2025-10-02 14:53:16 -0400

    updated REST API location

--- delivery/scripts/classes/plugins/org/craftercms/rd/plugin/mcp/client/McpSyncClient.groovy
+++ delivery/scripts/classes/plugins/org/craftercms/rd/plugin/mcp/client/McpSyncClient.groovy
@@ -101,7 +101,7 @@ class McpSyncClient {
         logger.info("Sending callTool request: ${objectMapper.writeValueAsString(request)}")
         
         def response = restClient.post()
-            .uri("mcp.json")
+            .uri("/api/plugins/org/craftercms/rd/plugin/mcp/server/craftermcp/mcp.json")
             .body(request)
             .retrieve()
             .toEntity(Map.class)
